6239aa27efd6abc03b36ddd22beca3240d802ad7
ccaisip-blackduck <ccaisip@blackduck.com>
2026-02-03 15:10:55 -0800

    Update SqlInjection.java - addl comment

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -12,7 +12,7 @@ public class SqlInjection {
 
     /* for demo, rename function */
     /* 9/12/2025 - no change for main branch test */
-    /* 02/03/2026 - rename for test */
+    /* 02/03/2026 - rename for test - addl comment to main*/
     public static ResultSet doQuery_demo_02032026(HttpServletRequest request, Connection connection) throws SQLException {
         String customerName = request.getParameter("customerName");
         String query = "SELECT account_balance FROM user_data WHERE user_name = " + customerName;
