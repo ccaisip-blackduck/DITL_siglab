05ea560701fe7ab89ba2b7039466913c376c5fd9
ccaisip-blackduck <ccaisip@blackduck.com>
2026-02-04 13:30:50 -0800

    commit ai insight fix suggestion

--- src/main/java/SqlInjection.java
+++ src/main/java/SqlInjection.java
@@ -13,6 +13,7 @@ public class SqlInjection {
     /* for demo, rename function */
     /* 9/12/2025 - no change for main branch test */
     /* 02/03/2026 - rename for test - addl comment to main*/
+    /* */
     public static ResultSet doQuery_demo_02032026(HttpServletRequest request, Connection connection) throws SQLException {
         String customerName = request.getParameter("customerName");
         String query = "SELECT account_balance FROM user_data WHERE user_name = " + customerName;
@@ -20,4 +21,18 @@ public class SqlInjection {
         ResultSet results = statement.executeQuery(query);
         return(results);
     }
+    
+
+
+     /* 02/03/2026 - rename for test - addl comment to main
+       AI Insights - recommended fix example: 
+     public static ResultSet doQuery_demo_02032026(HttpServletRequest request, Connection connection) throws SQLException {
+         String customerName = request.getParameter("customerName");
+         String query = "SELECT account_balance FROM user_data WHERE user_name = ?";
+         PreparedStatement preparedStatement = connection.prepareStatement(query);
+         preparedStatement.setString(1, customerName);
+         ResultSet results = preparedStatement.executeQuery();
+         return(results);
+    }
+    /* ccaisip - end of recommended fix */
 }
